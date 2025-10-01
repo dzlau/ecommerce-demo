50d7ef654d3755960806f9d3b12db1feaa567d73
Dennis <dennislzf@gmail.com>
2025-10-01 10:54:46 -0700

    added header

--- app/canada/page.tsx
+++ app/canada/page.tsx
@@ -39,7 +39,7 @@ export default async function CanadaPage() {
                         </div>
                     </div>
                 </div>
-
+                <h1>Welcome to canada</h1>
 
                 <h1 className="text-3xl font-bold mt-10">Top Prices for your Justin Bieber Concerts</h1>
                 <Image src={'/justin.webp'} alt="justin bieber" width={600} height={300} />
