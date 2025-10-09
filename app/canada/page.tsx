9f8fce560529709f0d30356501aebe258a29e235
Dennis <dennislzf@gmail.com>
2025-10-09 10:49:30 -0700

    remove

--- app/canada/page.tsx
+++ app/canada/page.tsx
@@ -40,9 +40,9 @@ export default async function CanadaPage() {
                     </div>
                 </div>
 
-                <h1 className="text-3xl font-bold mt-10">Top Prices for your Justin Bieber Concerts</h1>
-                <Image src={'/justin.webp'} alt="justin bieber" width={600} height={300} />
-                <h1>changed some code</h1>
+                {/* <h1 className="text-3xl font-bold mt-10">Top Prices for your Justin Bieber Concerts</h1>
+                <Image src={'/justin.webp'} alt="justin bieber" width={600} height={300} /> */}
+
             </main >
         </>
     )
