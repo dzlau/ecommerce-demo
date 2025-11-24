dfdcec81fdf5f3d6c1243c94c0fcce711463cf81
Dennis <dennislzf@gmail.com>
2025-11-24 14:27:06 -0800

    added link

--- app/contentful/page.tsx
+++ app/contentful/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header'
-import { getHeroContent } from '@/lib/contentful'   
+import { getHeroContent } from '@/lib/contentful'
+import { CONNREFUSED } from 'dns'
 import { draftMode } from 'next/headers'
 
 
@@ -9,13 +10,17 @@ export default async function ContentfulPage() {
 
     return (
         <>
-        <Header />
-        <main className="flex min-h-screen flex-col items-center bg-stone-100">
-            <div className="flex flex-col items-center justify-center">
+            <Header />
+            <main className="flex min-h-screen flex-col items-center bg-stone-100">
+                <div className="flex flex-col items-center justify-center">
                     <h1 className="text-4xl font-bold text-center my-10">{heroContent}</h1>
+
                 </div>
+                <p>Contentful link for Contentful requires a premium plan, watch full flow here: <a href='https://www.contentful.com/developers/docs/tools/vercel/content-source-maps-with-vercel/' target='_blank' rel='noopener noreferrer'>https://www.contentful.com/developers/docs/tools/vercel/content-source-maps-with-vercel/</a></p>
             </main>
 
         </>
     )
 }
+
+
