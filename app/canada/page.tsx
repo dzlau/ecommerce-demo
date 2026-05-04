85d13100d3f317334bd1363aa1bcad23d252a54d
Dennis <dennislzf@gmail.com>
2026-05-04 09:13:02 -0700

    remove title

--- app/canada/page.tsx
+++ app/canada/page.tsx
@@ -39,7 +39,6 @@ export default async function CanadaPage() {
                         </div>
                     </div>
                 </div>
-                <div>Welcome to Canada</div>
 
                 {/* <h1 className="text-3xl font-bold mt-10">Top Prices for your Justin Bieber Concerts</h1>
                 <Image src={'/justin.webp'} alt="justin bieber" width={600} height={300} /> */}
